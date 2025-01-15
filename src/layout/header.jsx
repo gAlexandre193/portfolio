8407142fb8352f9a73e25527cdacab02fa7cd91d
Gabriel Alexandre <gabrielalexandrerabelo@gmail.com>
2025-01-15 00:59:08 -0300

    Added effect to Header

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -1,12 +1,23 @@
+import { useState, useEffect } from 'react';
 import alexTechLogoImg from '../imgs/AlexTech.png';
 import styles from '../styles/header.module.css';
 
 export default function Header() {
+  const [showHeader, setShowHeader] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => setShowHeader(window.scrollY > 100);
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [])
+
   return (
-    <header className={styles.headerContainer}>
+    <header className={`${styles.headerContainer} ${showHeader ? styles.headerIsActive : ''}`}>
       <img 
         src={alexTechLogoImg}
-        alt="Alex Tech Logo"
+        alt='Alex Tech Logo'
         height={16}
       />
     </header>
